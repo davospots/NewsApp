e2c7d3bff0b5fe8c1598b95e228fd0e44e3c94c8
Dave <david.mathaga@student.moringaschool.com>
2021-10-30 02:38:06 +0300

    make code for top headlines

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    newsapi = NewsApiClient(api_key='599b8f291b15419aa4293f30f62920d2')
+
+    # top headlines
+    top_headlines = newsapi.get_top_headlines(country='ca')
+
+    t_articles = top_headlines['articles']
+    news = []
+    desc = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(t_articles)):
+        main_article = t_articles[i]
+
+        news.append(main_article['title'])
+        desc.append(main_article['description'])
+        img.append(main_article['urlToImage'])
+        p_date.append(main_article['publishedAt'])
+        url.append(main_article['url'])
+
+        contents = zip(news,desc,img,p_date,url)
+
+
     return render_template('index.html')
 
 
